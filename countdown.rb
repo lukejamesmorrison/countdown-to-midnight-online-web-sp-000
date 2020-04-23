297c689e8adb456beafbf7bf28f3de84066cc9c5
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 14:58:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  
+  until number == 0 do
+    number -= 1
+    puts "#{number+1} SECOND(S)!"
+  end
+  
+  return "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(number)
+  
+  until number == 0 do
+    number -= 1
+    puts "#{number+1} SECOND(S)!"
+    sleep(1)
+  end
+  
+  return "HAPPY NEW YEAR!"
 end
