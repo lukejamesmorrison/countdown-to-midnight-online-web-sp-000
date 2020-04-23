5f6158743ad9f3c8ba1b3c6293f306b2927a2ce6
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 14:51:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,10 @@
 
 def countdown(number)
   
-  while number != 0 do
-    puts "number SECOND(S)!"
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  
+  puts "HAPPY NEW YEAR!"
 end
